30a5b5a7a2cdd78ba0b481b6449cc1d83b88b597
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-17 15:32:47 +0300

    adding my hw2 task3

---
+++ homeWork_02/ex3_ArithmeticOperations/Maksim_Kliashchou/MathOperationDemo.java
@@ -0,0 +1,13 @@
+public class MathOperationDemo {
+    public static void main(String[] args) {
+        int number1 = 40, number2 = 12;
+
+        System.out.println("First number = " + number1);
+        System.out.println("Second number = " + number2);
+
+        System.out.println("Addition = " + (number1 + number2));
+        System.out.println("Substraction = " + (number1 - number2));
+        System.out.println("Multiplication = " + (number1 * number2));
+        System.out.println("Division = " + (number1 / number2));
+    }
+}
